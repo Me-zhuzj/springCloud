3d90c64c813b833008983cd6c7bf3556a9a66991
Administrator <Administrator@192.168.0.105>
2020-07-01 19:46:29 +0800

    sdf

--- product-view-service-feign/src/main/java/com/cn/feignView/ProductViewServiceFeignApplication.java
+++ product-view-service-feign/src/main/java/com/cn/feignView/ProductViewServiceFeignApplication.java
@@ -36,6 +36,7 @@ public class ProductViewServiceFeignApplication {
                     String strPort = scanner.nextLine();
                     if(!NumberUtil.isInteger(strPort)) {
                         System.err.println("只能是数字");
+                        System.out.println();
                         continue;
                     }
                     else {
